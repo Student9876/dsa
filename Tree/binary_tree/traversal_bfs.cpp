958311d10b037073d841039674f28c60673635cd
stdudent9876 <shouvik9876@gmail.com>
2024-02-17 01:02:11 +0530

    Add BFS code

---
+++ Tree/binary_tree/traversal_bfs.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define f(i,m,n) for(ll i=m;i<n;i++)
+#define fi(i,m,n) for(ll i=m;i>=n;i--)
+#define nl cout<<endl;
+#define ump(a,b) unordered_map<a,b>
+#define omp(a,b) map<a,b>
+const int MOD = 1e9+7;
+
+
+
+class TreeNode{
+    public:
+        int val;
+        TreeNode * left;
+        TreeNode * right;
+        TreeNode():val(0),left(nullptr),right(nullptr){}
+        TreeNode(int val):val(val),left(nullptr),right(nullptr){}
+        TreeNode(int val, TreeNode * left, TreeNode * right):val(val),left(left), right(right){}
+};
+
+vector<vector<int>> levelOrderTraversal(TreeNode * root){
+    vector<vector<int>> ans;
+    if(root == NULL) return ans;
+    queue<TreeNode *> q;
+    q.push(root);
+    while(!q.empty()){
+        int size = q.size();
+        vector<int> level;
+        f(i,0,size){
+            TreeNode *node = q.front();
+            q.pop();
+            if(node->left!=NULL) q.push(node->left);
+            if(node->right!=NULL) q.push(node->right);
+            level.push_back(node->val);
+        }
+        ans.push_back(level);
+    }
+    return ans;
+}
+// Time Complexity O(N)
+// Space Complexity O(N)
+
+
+int main(){
+    TreeNode * root = new TreeNode(1);
+    root->left= new TreeNode(2);
+    root->right= new TreeNode(3);
+
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(7);
+
+
+    vector<vector<int>> ans = levelOrderTraversal(root);
+    cout<<"Level Order Traversal";nl;
+    for(auto it:ans){
+        for(auto item:it) cout<<item<<" ";
+    }
+
+
+    return 0;
+}
