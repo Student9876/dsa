8ed1fc35269691f078c3d696e916db5f1298909f
stdudent9876 <shouvik9876@gmail.com>
2024-03-02 22:21:44 +0530

    Add height of BT

---
+++ Tree/binary_tree/height.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define f(i,m,n) for(ll i=m;i<n;i++)
+#define fi(i,m,n) for(ll i=m;i>=n;i--)
+#define fa(i,j) for(auto i:j)
+#define nl cout<<endl;
+#define ump(a,b) unordered_map<a,b>
+#define omp(a,b) map<a,b>
+const int MOD = 1e9+7;
+ll power(ll x, ll y){ll res = 1;while(y>0){if(y&1) res = (res*x);y = y>>1;x = (x*x);}return res;}
+
+class TreeNode{
+    public:
+        int val;
+        TreeNode * left;
+        TreeNode * right;
+        TreeNode():val(0),left(nullptr),right(nullptr){}
+        TreeNode(int val):val(val),left(nullptr),right(nullptr){}
+        TreeNode(int val, TreeNode * left, TreeNode * right):val(val),left(left), right(right){}
+};
+
+
+// TC = O(n)
+// SC = O(n)
+int maxDepth(TreeNode * root){
+    if(root == NULL) return 0;
+
+    int lh = maxDepth(root->left);
+    int rh = maxDepth(root->right);
+    return 1 + max(lh,rh);
+}
+
+
+int main(){
+    
+    TreeNode * root = new TreeNode(1);
+    root->left= new TreeNode(2);
+    root->right= new TreeNode(3);
+
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(7);
+
+    cout<<"Height of the tree is: "<<maxDepth(root);
+
+
+    return 0;
+}
