154dacafb62392711049f9225eda6587455b81cf
stdudent9876 <shouvik9876@gmail.com>
2024-03-02 23:05:28 +0530

    Add diameter of a binary tree code

--- Tree/binary_tree/height.cpp
+++ Tree/binary_tree/height.cpp
@@ -22,6 +22,9 @@ class TreeNode{
 };
 
 
+
+
+// Function for finding height of a tree 
 // TC = O(n)
 // SC = O(n)
 int maxDepth(TreeNode * root){
@@ -31,6 +34,40 @@ int maxDepth(TreeNode * root){
     int rh = maxDepth(root->right);
     return 1 + max(lh,rh);
 }
+
+
+
+// Function for checking a tree is balanced or not 
+// TC = O(n)
+// SC = O(n)
+int isBalanced(TreeNode * root){
+    if(root == NULL) return 0;
+
+    int lh = isBalanced(root->left);
+    if(lh == -1) return -1;
+    int rh = isBalanced(root->right);
+    if(rh == -1) return -1;
+
+    if(abs(lh-rh)>1) return -1;
+
+    return 1 + max(lh,rh);
+}
+
+
+// Function for diameter of a tree (longest path between 2 nodes)
+// TC = O(n)
+// SC = O(n)
+int diameterOfBinaryTree(TreeNode * root, int &diameter){
+    if(root == NULL) return 0;
+
+    int lh = diameterOfBinaryTree(root->left, diameter);
+    int rh = diameterOfBinaryTree(root->right, diameter);
+    diameter = max(diameter, lh + rh);
+
+    return 1+max(lh,rh);
+}
+
+
 
 
 int main(){
@@ -45,7 +82,23 @@ int main(){
     root->right->left = new TreeNode(6);
     root->right->right = new TreeNode(7);
 
+    // root->left->left->left = new TreeNode(8);
+    // root->left->left->left->left = new TreeNode(9);
+
+
     cout<<"Height of the tree is: "<<maxDepth(root);
+
+    nl;
+
+    int balanced = isBalanced(root);
+    if(balanced == -1) cout<<"Tree is not balanced";
+    else cout<<"Tree is balanced";
+    
+    nl;
+
+    int diameter=0;
+    diameterOfBinaryTree(root, diameter);
+    cout<<"Diameter of binary tree is: "<<diameter;
 
 
     return 0;
