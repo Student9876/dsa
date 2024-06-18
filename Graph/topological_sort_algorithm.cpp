7c3f11ca8097155779bf830894947b77658165eb
student9876 <shouvik9876@gmail.com>
2024-06-18 11:39:41 +0530

    Add topological sort BFS (Kahn's) Algorithm

--- Graph/topological_sort_algorithm.cpp
+++ Graph/topological_sort_algorithm.cpp
@@ -66,6 +66,47 @@ class Solution
 
 
 
+
+// Topological sort Algorithm using BFS 
+// Kahn's Algorithm 
+class Solution1
+{
+    
+public:
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    vector<int> indeg(V,0);
+	    
+	    for(int i=0;i<V;i++){
+	        for(auto it:adj[i]) indeg[it]++;
+	    }
+	    queue<int> q;
+	    for(int i=0;i<V;i++){
+	        if(indeg[i] == 0) q.push(i);
+	    }
+	    
+	    vector<int> topo;
+	   // Someone will always be in the queue because its a directed acyclic graph
+	   
+	   while(!q.empty()){
+	        int node = q.front();
+	        q.pop();
+	        
+	        topo.push_back(node);
+	        
+	        for(auto it:adj[node]){
+	            indeg[it]--;
+	            if(indeg[it] == 0) q.push(it);
+	        }
+	        
+	   }
+	   return topo;
+	    
+	}
+};
+
+
+
 int main(){
     int T;cin>>T;
     while(T--){
