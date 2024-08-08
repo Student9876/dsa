e1041d2e5b9405993961ba64154eed9a58658000
student9876 <shouvik9876@gmail.com>
2024-08-08 21:04:04 +0530

    add count the inversions

---
+++ Recursion/number-of-inversions.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define f(i,m,n) for(ll i=m;i<n;i++)
+#define fi(i,m,n) for(ll i=m;i>=n;i--)
+#define fa(i,j) for(auto i:j)
+#define nl cout<<endl;
+#define ump(a,b) unordered_map<a,b>
+#define omp(a,b) map<a,b>
+const int MOD = 1e9+7;
+ll power(ll x, ll y){ll res = 1;while(y>0){if(y&1) res = (res*x);y = y>>1;x = (x*x);}return res;}
+
+class TreeNode{
+    public:
+        int val;
+        TreeNode * left;
+        TreeNode * right;
+        TreeNode():val(0),left(nullptr),right(nullptr){}
+        TreeNode(int val):val(val),left(nullptr),right(nullptr){}
+        TreeNode(int val, TreeNode * left, TreeNode * right):val(val),left(left), right(right){}
+} typedef TN;
+
+
+int merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp;
+    int left = low;  
+    int right = mid + 1; 
+    int cnt = 0;
+
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else {
+            temp.push_back(arr[right]);
+            cnt += (mid - left + 1);
+            right++;
+        }
+    }
+
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+
+    return cnt;
+}
+
+int mergeSort(vector<int> &arr, int low, int high) {
+    int cnt = 0;
+    if (low >= high) return cnt;
+    int mid = (low + high) / 2 ;
+    cnt += mergeSort(arr, low, mid); 
+    cnt += mergeSort(arr, mid + 1, high);
+    cnt += merge(arr, low, mid, high); 
+    return cnt;
+}
+
+int numberOfInversions(vector<int>&a, int n) {
+    return mergeSort(a, 0, n - 1);
+}
+
+int main(){
+    int n;cin>>n;
+
+    vector<int> a(n);
+    f(i,0,n) cin>>a[i];
+    int cnt = numberOfInversions(a, n);
+    cout << cnt << endl;
+    return 0;
+}
+
