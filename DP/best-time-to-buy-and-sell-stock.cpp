22382b07401e149a04e6e4ac4fa1dfd6ccdf6b66
student9876 <shouvik9876@gmail.com>
2025-01-15 21:57:42 +0530

    Add best time to buy and sell stocks 1

---
+++ DP/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define f(i,m,n) for(ll i=m;i<n;i++)
+#define fi(i,m,n) for(ll i=m;i>=n;i--)
+#define fa(i,j) for(auto i:j)
+#define nl cout<<endl;
+#define ump(a,b) unordered_map<a,b>
+#define omp(a,b) map<a,b>
+const int MOD = 1e9+7;
+ll power(ll x, ll y){ll res = 1;while(y>0){if(y&1) res = (res*x);y = y>>1;x = (x*x);}return res;}
+
+int countSetBits(int u){unsigned int uCount;uCount = u - ((u >> 1) & 033333333333) - ((u >> 2) & 011111111111);return ((uCount + (uCount >> 3)) & 030707070707) % 63;}
+
+class TreeNode{
+    public:
+        int val;
+        TreeNode * left;
+        TreeNode * right;
+        TreeNode():val(0),left(nullptr),right(nullptr){}
+        TreeNode(int val):val(val),left(nullptr),right(nullptr){}
+        TreeNode(int val, TreeNode * left, TreeNode * right):val(val),left(left), right(right){}
+} typedef TN;
+
+
+/*
+    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+*/
+
+// Space Optimised 
+int maxProfit(vector<int>& prices) {
+    int ans = 0, mn = prices[0];
+    int n = prices.size();
+    for(int i = 1;i<n;i++){
+        int price = prices[i] - mn;
+        ans = max(ans, price);
+        mn = min(mn, prices[i]);
+    }
+    return ans;
+}
+
+
+int main(){
+    int T;cin>>T;
+    while(T--){
+    
+    }
+    return 0;
+}
