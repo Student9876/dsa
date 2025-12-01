3847a37bdc54da43bee00f0fe443c59c6e975b04
student9876 <shouvik9876@gmail.com>
2025-12-01 19:07:02 +0530

    add another solution

--- Graph/detect_cycle_BFS_graph.cpp
+++ Graph/detect_cycle_BFS_graph.cpp
@@ -58,6 +58,48 @@ class Solution {
 };
 
 
+
+class Solution1 {
+  public:
+    bool detect(int src, vector<int> adj[], vector<int> &vis){
+        vis[src] = 1;
+        queue<pair<int, int>> q;
+        q.push({src, -1});
+        
+        while(!q.empty()){
+            int node = q.front().first;
+            int parent = q.front().second;
+            q.pop();
+            
+            for(auto adjacentNode: adj[node]){
+                if(!vis[adjacentNode]){
+                    vis[adjacentNode] = 1;
+                    q.push({adjacentNode, node});
+                } else if(parent != adjacentNode){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    bool isCycle(int V, vector<vector<int>>& edges) {
+        vector<int> vis(V, 0);
+        vector<int> adj[V];
+        for(auto x:edges){
+            adj[x[0]].push_back(x[1]);
+            adj[x[1]].push_back(x[0]);
+        }
+        for(int i = 0;i<V;i++){
+            if(!vis[i])
+                if(detect(i, adj, vis)) return true;
+        }
+        return false;
+        
+    }
+};
+
+
 int main(){
     int T;cin>>T;
     while(T--){
